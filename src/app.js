2236b2ce41069843d2e35038c28680352d82fbb7
Lyndsay-alt <richardslyndsay@gmail.com>
2025-08-28 06:22:47 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const who = ['The shark', 'The grizzly', 'The wolf', 'Godzilla'];
+const action = ['broke apart', 'tore open', 'crushed', 'carried away'];
+const what = ['my leg', 'my arm', 'my body', 'my foot'];
+const when = [
+  'for no reason!',
+  'when I was relaxing!',
+  'while I was hanging out with my friends!',
+  'while I was on vacation!'
+];
+
+
+function pick(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+
+function generateExcuse() {
+  return `${pick(who)} ${pick(action)} ${pick(what)} ${pick(when)}`;
+}
+
+
+window.onload = () => {
+  const excuseEl = document.getElementById('excuse');     
+  const btn = document.getElementById('newExcuse');       
+
+
+  excuseEl.innerHTML = generateExcuse();
+
+
+  btn.addEventListener("click", () => {
+    excuseEl.innerHTML = generateExcuse();
+    excuseEl.style.color = randomColor();
+  });
 };
